b9748e9700ce4c141b208ef2a0f16b0485a2490b
olimov Amir <olimov.amir@mail.ru>
2024-03-20 07:35:51 +0300

    Add project files.

---
+++ Infrastructure/PostgreSQLDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SaleOfProductsJWT.Infrastructure
+{
+    public class PostgreSQLDbContext :DbContext
+    {
+        public PostgreSQLDbContext(DbContextOptions<PostgreSQLDbContext> options) : base(options)
+        {
+            Database.Migrate();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repositories/PostgreSQLRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using SaleOfProductsJWT.Infrastructure;
+
+namespace SaleOfProductsJWT.Repositories
+{
+    public class PostgreSQLRepository<T> : IPostgreSQLRepository<T>
+    {
+        readonly PostgreSQLDbContext _context;
+        public PostgreSQLRepository(PostgreSQLDbContext bankContext)
+        {
+            _context = bankContext;
+        }
+        public bool Create(T item)
+        {
+            try
+            {
+                _context.Add(item);
+                var result = _context.SaveChanges();
+                return result > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T item)
+        {
+            try
+            {
+                _context.Update(item);
+                var result = _context.SaveChanges();
+                return result > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
