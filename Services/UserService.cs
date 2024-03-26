d3bd94ee5f17f9afe42c9b8fedd6b92fba4da09f
olimov Amir <olimov.amir@mail.ru>
2024-03-26 21:08:46 +0300

    fix

---
+++ Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProductsJWT.Models;
+
+namespace SaleOfProductsJWT.Services
+{
+    public class UserService : IUserService
+    {
+        public string Create(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, User item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
