a6c4c17d2aaac6a65a9a9dffac400d6b7bc0d89a
olimov Amir <olimov.amir@mail.ru>
2024-03-26 14:50:57 +0300

    fix

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,29 @@
-﻿namespace SaleOfProductsJWT.Models
+﻿using SaleOfProductsJWT.Models.BaseClassModels;
+
+namespace SaleOfProductsJWT.Models
 {
-    public class User
+    public class User : BaseEntity
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(Guid id, string name, string email, string password )
+        {
+            Id = id;
+            Name = name;
+            Email = email; 
+            Password = password;
+        }
+
+        public static User Create(Guid id, string name, string email, string password)
+        {
+            return new User(id, name, email, password);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Email} {Password}";
+        }
     }
 }
