57b55a733de37250d95e546f34c5bb37c4c4a854
olimov Amir <olimov.amir@mail.ru>
2024-03-27 19:50:06 +0300

    fix

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProductsJWT.Models.BaseClassModels;
+using SaleOfProductsJWT.Services;
+
+namespace SaleOfProductsJWT.Controllers
+{
+    public abstract class BaseController<TEntity> : ControllerBase where TEntity : BaseEntity
+    {
+        protected readonly IBaseService<TEntity> _service;
+        protected readonly ILogger<BaseController<TEntity>> _logger;
+        public BaseController(ILogger<BaseController<TEntity>> logger, IBaseService<TEntity> service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+    }
+}
