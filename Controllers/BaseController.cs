e5a4900311b3e3918f8dbb49b78435eecbab9e3b
olimov Amir <olimov.amir@mail.ru>
2024-03-27 19:50:37 +0300

    fix

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -14,5 +14,37 @@ namespace SaleOfProductsJWT.Controllers
             _service = service;
         }
 
+        [HttpGet("AllItems")]
+        public virtual IEnumerable<TEntity> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public virtual TEntity Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public virtual string Post([FromBody] TEntity item)
+        {
+
+
+            return _service.Create(item);
+
+        }
+
+        [HttpPut("Update")]
+        public virtual string Put([FromQuery] Guid id, [FromBody] TEntity item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public virtual string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
+        }
     }
 }
