456be67f8ace86fa7b999ff841b50514aa4e7984
olimov Amir <olimov.amir@mail.ru>
2024-03-28 14:06:22 +0300

    fix

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProductsJWT.Models;
+using SaleOfProductsJWT.Services;
+
+namespace SaleOfProductsJWT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IUserService _userService;
+
+        public AuthenticationController(IConfiguration config, IUserService userService)
+        {
+            _config = config;
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest model)
+        {
+            var user = _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+    }
+}
