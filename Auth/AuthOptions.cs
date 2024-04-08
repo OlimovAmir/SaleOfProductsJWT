03f5d3d88b7be8f1c4e30d6f24dd2c1dfca117df
olimov Amir <olimov.amir@mail.ru>
2024-04-08 16:59:33 +0300

    fix

--- Auth/AuthOptions.cs
+++ Auth/AuthOptions.cs
@@ -1,6 +1,15 @@
-﻿namespace SaleOfProductsJWT.Auth
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SaleOfProductsJWT.Auth
 {
     public class AuthOptions
     {
+        public const string ISSUER = "BankServer";
+        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+        const string KEY = "2EC1EE51-1100-4347-BF22-EEB6CB8B0695";   // ключ для шифрации
+        public const int LIFETIME = 30; // время жизни токена - 15 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
     }
 }
