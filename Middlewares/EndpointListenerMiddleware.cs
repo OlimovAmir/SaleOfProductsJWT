df4255aa2ac51ac314dace6d8010657a587dfc04
olimov Amir <olimov.amir@mail.ru>
2024-04-08 17:12:31 +0300

    fix bag

--- Middlewares/EndpointListenerMiddleware.cs
+++ Middlewares/EndpointListenerMiddleware.cs
@@ -2,5 +2,20 @@
 {
     public class EndpointListenerMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<EndpointListenerMiddleware> _logger;
+
+        public EndpointListenerMiddleware(RequestDelegate next, ILogger<EndpointListenerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            _logger.LogInformation(httpContext.Request.Path.Value);
+
+            await _next(httpContext);
+        }
     }
 }
