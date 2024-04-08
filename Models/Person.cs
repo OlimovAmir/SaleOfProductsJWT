d15c6997fdaf402aa7e965a829a3b901dec997ca
olimov Amir <olimov.amir@mail.ru>
2024-04-08 16:55:50 +0300

    fix

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,7 +1,27 @@
-﻿namespace SaleOfProductsJWT.Models
-{
-    public class Person
-    {
+﻿using SaleOfProductsJWT.Models.BaseClassModels;
 
+namespace SaleOfProductsJWT.Models
+{
+    public class Person : BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTimeOffset Birthday { get; set; }
+
+        public bool IsBlocked { get; }
     }
 }
