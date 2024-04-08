80cd54b471a46e68351b2394a648d68cebc44831
olimov Amir <olimov.amir@mail.ru>
2024-04-08 16:44:40 +0300

    fix

--- Services/ServiceCollectionExtensions.cs
+++ Services/ServiceCollectionExtensions.cs
@@ -1,7 +1,15 @@
-﻿namespace SaleOfProductsJWT.Services
-{
-    public class ServiceCollectionExtensions
-    {
+﻿using SaleOfProductsJWT.Repositories;
 
+namespace SaleOfProductsJWT.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddMyServices(this IServiceCollection service)
+        {
+            service.AddScoped<AuthService>();
+           
+
+            service.AddScoped(typeof(IPostgreSQLRepository<>), typeof(PostgreSQLRepository<>));
+        }
     }
 }
