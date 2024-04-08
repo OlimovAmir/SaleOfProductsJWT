21f191e78cc33e6b204c1d7e1d7600a89f97206b
olimov Amir <olimov.amir@mail.ru>
2024-04-08 17:11:58 +0300

    fix bag

--- Middlewares/ApplicationKeyMiddleware.cs
+++ Middlewares/ApplicationKeyMiddleware.cs
@@ -2,5 +2,20 @@
 {
     public class ApplicationKeyMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ApplicationKeyMiddleware> _logger;
+
+        public ApplicationKeyMiddleware(RequestDelegate next, ILogger<ApplicationKeyMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            
+            await _next(httpContext);
+            
+        }
     }
 }
