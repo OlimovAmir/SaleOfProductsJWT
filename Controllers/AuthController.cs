cc42b9cafc74d3b54f3164e959b62035fa829f3e
olimov Amir <olimov.amir@mail.ru>
2024-04-08 17:23:46 +0300

    fix bag

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using SaleOfProductsJWT.Contracts;
 using SaleOfProductsJWT.Models;
 using SaleOfProductsJWT.Services;
 using System.IdentityModel.Tokens.Jwt;
@@ -12,47 +13,38 @@ namespace SaleOfProductsJWT.Controllers
     [Route("[controller]")]
     public class AuthController : ControllerBase
     {
-        private readonly IConfiguration _config;
-        private readonly IUserService _userService;
-
-        public AuthController(IConfiguration config, IUserService userService)
+        protected readonly AuthService _authService;
+        public AuthController(AuthService authService)
         {
-            _config = config;
-            _userService = userService;
+            _authService = authService;
         }
 
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody] AuthenticateRequest model)
+        [HttpPost("Login")]
+        public async Task<IActionResult> LogIn(string username, string password)
         {
-            var user = _userService.Authenticate(model.Username, model.Password);
-
-            if (user == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
-
-            // Генерация JWT токена
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_config["Jwt:Secret"]);
-            var tokenDescriptor = new SecurityTokenDescriptor
+            try
             {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                new Claim(ClaimTypes.Name, user.Id.ToString()),
-                new Claim(ClaimTypes.Role, user.Role)
-                }),
-                Expires = DateTime.UtcNow.AddHours(1), // Токен истекает через 1 час
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-
-            // Возвращаем результат аутентификации с токеном
-            return Ok(new
+                TokenInfo token = await _authService.Login(username, password);
+                return Ok(token);
+            }
+            catch (Exception ex)
             {
-                user.Id,
-                user.Name,
-                user.Role,
-                Token = tokenString // Добавляем токен к результату аутентификации
-            });
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("RefreshToken")]
+        public async Task<IActionResult> RefreshToken(string refreshToken)
+        {
+            try
+            {
+                TokenInfo token = await _authService.RefreshToken(refreshToken);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
