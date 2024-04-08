7ffcb046a846ca20641dd8cdd7d0cc8d8da93c6e
olimov Amir <olimov.amir@mail.ru>
2024-04-08 17:00:20 +0300

    fix bag

--- Auth/ServiceCollectionExtensions.cs
+++ Auth/ServiceCollectionExtensions.cs
@@ -1,6 +1,37 @@
-﻿namespace SaleOfProductsJWT.Auth
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SaleOfProductsJWT.Auth
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static void AddMyAuth(this IServiceCollection service)
+        {
+            service.AddAuthorization();
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // указывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOptions.ISSUER,
+
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOptions.AUDIENCE,
+
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                    };
+                });
+        }
     }
 }
