d5cc222c59af52c05ee9e296763edbb6ce6569d8
olimov Amir <olimov.amir@mail.ru>
2024-04-16 13:12:20 +0300

    fix

--- Auth/AuthOptions.cs
+++ Auth/AuthOptions.cs
@@ -5,8 +5,8 @@ namespace SaleOfProductsJWT.Auth
 {
     public class AuthOptions
     {
-        public const string ISSUER = "BankServer";
-        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+        public const string ISSUER = "SaleOfProductJWT";
+        public const string AUDIENCE = "MyAuthUser"; // потребитель токена
         const string KEY = "2EC1EE51-1100-4347-BF22-EEB6CB8B0695";   // ключ для шифрации
         public const int LIFETIME = 30; // время жизни токена - 15 минута
         public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
