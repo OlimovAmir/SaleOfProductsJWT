eff68c35455184bde502e413518302580ae30826
olimov Amir <olimov.amir@mail.ru>
2024-04-18 14:25:25 +0300

    fix

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SaleOfProductsJWT.Models;
+using SaleOfProductsJWT.Models.UserDTO;
+using SaleOfProductsJWT.Services;
 using SaleOfProductsJWT.Services.IService;
 
 namespace SaleOfProductsJWT.Controllers
@@ -17,6 +19,13 @@ namespace SaleOfProductsJWT.Controllers
             _mapper = mapper;
         }
 
-        
+       
+        [HttpGet("AllUSER")]
+        public virtual IEnumerable<UserDTO> Get()
+        {
+            var users = _service.GetAll();
+            var userDTOs = _mapper.Map<IEnumerable<UserDTO>>(users);
+            return userDTOs;
+        }
     }
 }
