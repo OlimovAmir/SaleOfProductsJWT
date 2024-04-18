270aaf291da0774ba8155a16a72fa68e25202826
olimov Amir <olimov.amir@mail.ru>
2024-04-18 13:32:36 +0300

    fix

--- Mappings/UseMappings.cs
+++ Mappings/UseMappings.cs
@@ -1,7 +1,17 @@
-﻿namespace SaleOfProductsJWT.Mappings
+﻿using AutoMapper;
+using SaleOfProductsJWT.Models.UserDTO;
+
+namespace SaleOfProductsJWT.Mappings
 {
-    public class UseMappings
+    public class UseMappings : Profile
     {
 
+        public UseMappings()
+        {
+            CreateMap<User, UserDTO>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+            // Добавьте другие настройки для остальных полей
+        }
     }
 }
