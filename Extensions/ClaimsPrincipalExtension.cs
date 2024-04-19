99db3f70b12196aa18b27057f8cdfaa87d337f2b
olimov Amir <olimov.amir@mail.ru>
2024-04-19 20:29:29 +0300

    fix

--- Extensions/ClaimsPrincipalExtension.cs
+++ Extensions/ClaimsPrincipalExtension.cs
@@ -1,7 +1,16 @@
-﻿namespace SaleOfProductsJWT.Extensions
-{
-    public class ClaimsPrincipalExtension
-    {
+﻿using System.Security.Claims;
 
+namespace SaleOfProductsJWT.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static Guid GetCurrectUserId(this ClaimsPrincipal principal)
+        {
+            var role = principal?.Claims.FirstOrDefault(c => c.Type == "Id")?.Value;
+            if (Guid.TryParse(role, out Guid userId))
+                return userId;
+
+            return Guid.Empty;
+        }
     }
 }
