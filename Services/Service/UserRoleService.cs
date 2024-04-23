b5285311e419589c324222f57c314419d255e9d9
olimov Amir <olimov.amir@mail.ru>
2024-04-23 15:08:33 +0300

    fix

--- Services/Service/UserRoleService.cs
+++ Services/Service/UserRoleService.cs
@@ -1,6 +1,33 @@
-﻿namespace SaleOfProductsJWT.Services.Service
+﻿using SaleOfProductsJWT.Models;
+using SaleOfProductsJWT.Services.IService;
+
+namespace SaleOfProductsJWT.Services.Service
 {
-    public class UserRoleService
+    public class UserRoleService : IUserRoleService
     {
+        public string Create(UserRole item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<UserRole> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserRole GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, UserRole item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
